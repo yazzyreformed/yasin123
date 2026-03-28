fbf4f65b02ad15bcd531e289d0c795eb67484804
Yasin Zemheri <slmnysnzmhr@hotmail.com>
2026-03-28 18:57:26 +0300

    fix: default tema light mode olarak guncellendi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
       <body className="font-sans min-h-full flex flex-col">
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
